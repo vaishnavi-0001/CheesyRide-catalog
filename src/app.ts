1b08817dbee4c8da15384054c266847c7a45cd79
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-23 20:12:10 +0530

    Add Authentication

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Hello from catalog service!" });
